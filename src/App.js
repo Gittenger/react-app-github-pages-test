3ad04549a8d8b3ddb0acbb1d9a59148b71d984c1
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-15 00:09:07 -0400

    testing routing in prod

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 const Content = () => (
 	<div style={{ textAlign: 'center' }}>
@@ -7,6 +7,7 @@ const Content = () => (
 			<p>
 				Edit <code>src/App.js</code> and save to reload.
 			</p>
+			<Link to="/about">About Link</Link>
 			<a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 				Learn React
 			</a>
@@ -14,12 +15,17 @@ const Content = () => (
 	</div>
 )
 
+const About = () => <div style={{ textAlign: 'center' }}>ABOUT PAGE</div>
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/">
 					<Route index element={<Content />} />
+				</Route>
+				<Route path="/about">
+					<Route index element={<About />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
