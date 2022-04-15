7f5601d7cc2d79ef032bbd9717609afa1c2f7b03
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-15 00:05:47 -0400

    implement router basic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Content = () => (
+	<div style={{ textAlign: 'center' }}>
+		<header>
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+			<a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+				Learn React
+			</a>
+		</header>
+	</div>
+)
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Route index element={<Content />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
